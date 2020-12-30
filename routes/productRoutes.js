e1c0ffab878e6bc041bb480195e696ec4473a256
mohit-cdac <mohitvani.cdac@gmail.com>
2020-12-30 20:38:34 +0530

    added product model and product routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,5 +1,23 @@
-import express from 'express'
-const router = express.Router()
+import express from "express";
+import {
+  getProducts,
+  getTopProducts,
+  getProductsById,
+  createProduct,
+  deleteProduct,
+  updateProduct,
+  createProductReview
+} from "../controller/productController.js";
+import { protect , admin } from "../middleware/authMiddleware.js";
+const router = express.Router();
 
+router.get("/", getProducts);
+router.get("/top", getTopProducts);
+router.get("/:id", getProductsById);
+router.delete("/:id", protect , admin , deleteProduct);
+router.post('/',protect, admin, createProduct);
+router.put('/:id',protect, admin, updateProduct)
 
-export default router
+router.post('/:id/reviews' ,protect, createProductReview)
+
+export default router;
