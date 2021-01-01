042f6dfeae761767fa16bcbba89ae9bec8182be5
Brillfox <rishabharya657@gmail.com>
2021-01-01 14:49:41 +0530

    adding order model

--- controller/orderController.js
+++ controller/orderController.js
@@ -1,5 +1,17 @@
-import Product from "../models/productModel.js";
+import Order from "../models/orderModel.js";
 import asyncHandler from "express-async-handler";
 
+export const getOrderById = asyncHandler(async (req, res, next) => {
+    let order 
+    try {
+        order = await Order.findById(req.params.id);
+        if(order == null) {
+            return res.status(404).json({ message: 'cannot find the order' })
+        }
+    } catch (err) {
+        return res.status(500).json({ message: err.message })
+    }
+    res.json(order)
+})
 
 
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -7,22 +7,8 @@ import { protect, admin } from '../middleware/authMiddleware.js'
 
 
 
-router.get('/:id', getOrderById, (req, res) => {
-    res.json(res.order)
-    });
-    async function getOrderById (req, res, next) {
-        let order 
-        try {
-            order = await Order.findById(req.params.id);
-            if(order == null) {
-                return res.status(404).json({ message: 'cannot find the order' })
-            }
-        } catch (err) {
-            return res.status(500).json({ message: err.message })
-        }
-        res.order = order
-        next();
-    }
+router.get('/:id', getOrderById);
+     
         
 
 
